c694284900d246a93693b1e9537ec42377eba9a7
Kristjan Jansen <kristjan.jansen@gmail.com>
2013-02-07 18:43:34 +0200

    simpler

--- app.js
+++ app.js
@@ -3,14 +3,21 @@ var fs = require('fs');
 var tako = require('tako')
 var app = tako()
 
+var midi = require('midi');
+
 app.route('/').file(__dirname + '/client/index.html');
 app.route('/*').files(__dirname + '/client');
 
 app.httpServer.listen(8000)
 
+var input = new midi.input();
+input.openVirtualPort("Test Input");
+
 app.sockets.on('connection', function (socket) {
-  setTimeout(function() {
-    app.sockets.emit('message', { position: 0});
-  }, 10000)
+  
+  input.on('message', function(deltaTime, message) {
+    app.sockets.emit('message', { key: message[1], vol: message[2]});
+    console.log(message)
+  });
 
 })
--- client/js/app.js
+++ client/js/app.js
@@ -3,43 +3,33 @@ $(function() {
 var keys = {
   72  : {
     'video': 'http://www.youtube.com/watch?v=oHg5SJYRHA0',
-    'start': 4,
-    'stop': 10
+    'start': 4
   },
   74  : {
     'video': 'http://www.youtube.com/watch?v=yBwD4iYcWC4',
-    'start': 84,
-    'stop': 87
+    'start': 84
   }
 }
 
 for (var key in keys) {
-  keys[key].popcorn = new Popcorn.youtube('#video', keys[key].video).cue(keys[key].stop, function() {
-    this.currentTime(keys[key].start)
-    this.unmute()
-  });
+  keys[key].popcorn = new Popcorn.youtube('#video', keys[key].video)
 };
 
 
 var socket = io.connect('http://localhost:8000');
 
 socket.on('message', function (data) {
-//  console.log(data)
   if (keys[data.key]) {
- //   keys[data.key].popcorn.currentTime()
-    console.log(keys[data.key].start)
-    keys[data.key].popcorn.play(keys[data.key].start);
     if (data.vol > 0) {
       keys[data.key].popcorn.unmute();
-      keys[data.key].popcorn.play()
+      keys[data.key].popcorn.currentTime(keys[data.key].start);
+      keys[data.key].popcorn.play();
     } else {
       keys[data.key].popcorn.mute();
       keys[data.key].popcorn.pause();
     }
   }
 });
-
-
 
 
 })
