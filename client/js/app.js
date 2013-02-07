906020532975ce6dce5f5ccb583e5f181fc936b5
Kristjan Jansen <kristjan.jansen@gmail.com>
2013-02-07 18:38:09 +0200

    cue

--- client/js/app.js
+++ client/js/app.js
@@ -3,26 +3,32 @@ $(function() {
 var keys = {
   72  : {
     'video': 'http://www.youtube.com/watch?v=oHg5SJYRHA0',
-    'time': 4
+    'start': 4,
+    'stop': 10
   },
   74  : {
     'video': 'http://www.youtube.com/watch?v=yBwD4iYcWC4',
-    'time': 82
+    'start': 84,
+    'stop': 87
   }
 }
 
 for (var key in keys) {
-  keys[key].popcorn = new Popcorn.youtube('#video', keys[key].video)
+  keys[key].popcorn = new Popcorn.youtube('#video', keys[key].video).cue(keys[key].stop, function() {
+    this.currentTime(keys[key].start)
+    this.unmute()
+  });
 };
 
 
 var socket = io.connect('http://localhost:8000');
 
 socket.on('message', function (data) {
-  console.log(data)
+//  console.log(data)
   if (keys[data.key]) {
-    keys[data.key].popcorn.currentTime(keys[data.key].time)
-    keys[data.key].popcorn.play();
+ //   keys[data.key].popcorn.currentTime()
+    console.log(keys[data.key].start)
+    keys[data.key].popcorn.play(keys[data.key].start);
     if (data.vol > 0) {
       keys[data.key].popcorn.unmute();
       keys[data.key].popcorn.play()
